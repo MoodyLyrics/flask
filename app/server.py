7f97832ca6a013fb7aee3584e655f208eb9963cf
Hubert Theodore <hubert@yup.com>
2016-04-25 23:26:24 -0500

    port

--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, render_template
 from wordnik import swagger, WordApi
 from sets import Set
@@ -46,4 +47,5 @@ def synonym(word):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)

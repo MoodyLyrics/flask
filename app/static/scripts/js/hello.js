6b4d745d975c0febe7afeb68d2b75c29ac03487d
Hubert Theodore <hubert@yup.com>
2016-05-07 12:17:39 -0500

    https

--- app/static/scripts/js/hello.js
+++ app/static/scripts/js/hello.js
@@ -39,7 +39,7 @@ function searchRelatedWords(word) {
       $.each(res.data, function (key, val) {
         relatedWords.push(val);
       });
-      $.getJSON("localhost:9200/songs/lyrics/_search?q=" + relatedWords.join(","),
+      $.getJSON("https://192.241.182.160:9200/songs/lyrics/_search?q=" + relatedWords.join(","),
        function (res) {
         clearInterval(intervalBeep);
         sliceBeep = 0;
